515bbebed2cf05a99bfd6a3204777d5084a4a9a7
David Gunner Jnr <gunnerjnr@live.co.uk>
2017-10-05 11:41:20 +0100

    Added the files that make up the Band-Site for the final project, I was not aware that the final projects needed their own repo at the time and so I originally added it to https://github.com/GunnerJnr/_CodeInstitute/tree/master/Stream-1/Front-End-Development/Final-Project, so if you need more commit history please see this link

---
+++ Band-Project/js/app.js
@@ -0,0 +1,30 @@
+// define our application
+angular.module("bandApp", ['ngRoute', 'bandAppControllers', 'bandAppDirectives'])
+	.config(function ($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);  // enable href routing without hashes
+		// route our application
+		$routeProvider
+			.when('/home', {
+				templateUrl: "templates/home.html",
+				controller: "HomeController"
+			})
+			.when('/meet-the-band', {
+				templateUrl: "templates/meet-the-band.html",
+				controller: "AudioController"
+			})
+			.when('/tour-info', {
+				templateUrl: "templates/tour-info.html",
+				controller: "TourController"
+			})
+			.when('/services', {
+				templateUrl: "templates/services.html",
+				controller: "ContactController"
+			})
+			.when('/searchiTunes', {
+				templateUrl: "templates/search-itunes.html",
+				controller: "ItunesController"
+			})
+			.otherwise({
+				redirectTo: '/home'
+			});
+	});
